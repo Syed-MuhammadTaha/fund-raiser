17b306c932507c9b7192a48f0f6e32a7ed744b09
Abdul Moeez Asad <113182819+AMoeez1910@users.noreply.github.com>
2024-03-02 19:07:09 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import Profile from './pages/Profile'
 import axios from 'axios'
 import { UserContextProvider } from '../context/userContext'
 import Donate from './pages/Donate'
-axios.defaults.baseURL =process.env.REACT_APP_BACKEND_URL;
+axios.defaults.baseURL =`https://fund-raiser-production.up.railway.app`;
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import CheckoutSuccess from './pages/CheckoutSuccess'
