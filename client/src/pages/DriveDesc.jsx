36b205c68d17613bdbadc0d230f3d3893ce86169
Abdul Moeez Asad <113182819+AMoeez1910@users.noreply.github.com>
2024-03-02 20:15:44 +0500

    Update DriveDesc.jsx

--- client/src/pages/DriveDesc.jsx
+++ client/src/pages/DriveDesc.jsx
@@ -17,7 +17,7 @@ const DriveDesc = ({ onNext, onPrev, submitData }) => {
   
   axios.defaults.withCredentials=true
   useEffect(() => {
-    axios.get(`https://fund-raiser-production.up.railway.app//profile`)
+    axios.get(`https://fund-raiser-production.up.railway.app/profile`)
     .then(res => {
       if(res.data.Status === "Success"){
         setIsLoggedIn(true)
